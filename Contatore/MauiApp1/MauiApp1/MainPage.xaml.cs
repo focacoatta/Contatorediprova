5189e2e719cd1ed2cf62720939fccce06244c309
focacoatta <155883587+focacoatta@users.noreply.github.com>
2024-01-07 12:41:44 +0100

    Add files via upload

---
+++ Contatore/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private int contatore = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            AggiornaVisibilitaImmagini();
+        }
+
+        private void AggiornaVisibilitaImmagini()
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                NascondiImmagine(i);
+            }
+
+            MostraImmagine(contatore);
+        }
+
+        private void NascondiImmagine(int numero)
+        {
+            string nomeImmagine = $"{numero}4";
+            Image immagine = this.FindByName<Image>(nomeImmagine);
+
+            if (immagine != null)
+            {
+                immagine.IsVisible = false;
+            }
+        }
+
+        private void MostraImmagine(int numero)
+        {
+            string nomeImmagine = $"{numero}4";
+            Image immagine = this.FindByName<Image>(nomeImmagine);
+
+            if (immagine != null)
+            {
+                immagine.IsVisible = true;
+            }
+        }
+
+        private void OnContatorediProvaClicked(object sender, EventArgs e)
+        {
+            contatore = (contatore + 1) % 10;
+            AggiornaVisibilitaImmagini();
+        }
+    }
+}
